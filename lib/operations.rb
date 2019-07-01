21f430d997dd083d68aec6eeac534a42d6a6e90f
Andres Carvajal <jc1993box@hotmail.com>
2019-07-01 22:14:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
 
-end
+  if speed < 40 || speed > 60
+		true
+	elseif speed  < 30 || speed > 95
+		false
+	end
+	end
 
 
-
-def not_safe?(speed)
+#def not_safe?(speed)
 	
-end
+#end
 	
 
 
